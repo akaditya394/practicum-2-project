67c9284f6d14c07a34479aaf931020c61c006dba
Aditya <akaditya394@gmail.com>
2022-02-26 18:23:12 +0530

    sections created

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Section from "../src/components/welcome page components/Section";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div>
+        <Section />
+        <Section />
+        <Section />
+        <Section />
+        <Section />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ Client/src/components/welcome page components/Section.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Section.css";
+
+const Section = () => {
+  return <div className="section_box">section</div>;
+};
+
+export default Section;
