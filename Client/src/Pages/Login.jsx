93d9e2f085bdac87d58d5ecb8b735c991a0dad9a
Aditya <akaditya394@gmail.com>
2022-02-27 18:19:53 +0530

    login page added

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
+import Button from "../components/UI/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login_parent">
+      <div className="loginsignup_message">
+        <h1>login or signup...</h1>
+      </div>
+      <div className="loginsignup_auth">
+          <div>
+            <Button>
+              <FontAwesomeIcon icon="fa-brands fa-google" /> Sign up with Google
+            </Button>
+          </div>
+          <div>
+            <Button>Login with Google</Button>
+          </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
