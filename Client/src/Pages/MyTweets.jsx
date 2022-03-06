2773b2c6b0439b3a7631967f1c13566dfe6e526c
Aditya <akaditya394@gmail.com>
2022-03-06 11:17:06 +0530

    my tweets conatiner created

--- Client/src/Pages/MyTweets.jsx
+++ Client/src/Pages/MyTweets.jsx
@@ -4,9 +4,11 @@ import "./MyTweets.css";
 const MyTweets = () => {
   return (
     <div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="mytweets_main">
+        <div className="mytweets_left_container"></div>
+        <div className="mytweets_middle_container"></div>
+        <div className="mytweets_right_container"></div>
+      </div>
     </div>
   );
 };
