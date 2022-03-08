0ec9d0c53917066291efd7201ed6aa05bdecf31b
Aditya <akaditya394@gmail.com>
2022-03-08 19:08:51 +0530

    ADD TWEET BUTTON ADDED

--- Client/src/components/UI/CreateTweet.jsx
+++ Client/src/components/UI/CreateTweet.jsx
@@ -5,18 +5,18 @@ import "./CreateTweet.css";
 const CreateTweet = () => {
   return (
     <div>
-      <div>
+      <div className="createtweet_main">
         <form>
           <div>
             <textarea
               className="createtweet_textarea"
               placeholder="what's on your mind?"
               rows="6"
-              cols="93"
+              cols="90"
             />
           </div>
           <div>
-            <Button>Submit</Button>
+            <Button className="addtweet_button">tweet</Button>
           </div>
         </form>
       </div>
