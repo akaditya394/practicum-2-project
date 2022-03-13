5d47062b8000619aad91ff6ce97fbb51293b5a40
Aditya <akaditya394@gmail.com>
2022-03-13 17:49:04 +0530

    view profile page done

--- Client/src/Pages/Profile.jsx
+++ Client/src/Pages/Profile.jsx
@@ -9,14 +9,41 @@ const Profile = () => {
       <div className="profile-container">
         <div className="profile-main">
           <div className="profile-upper">
-            <div className="profile-image-div"></div>
-            <div className="profile-name-bio"></div>
+            <div className="profile-image-div">
+              <img
+              className="profile-image"
+                src="https://nettv4u.com/imagine/23-10-2019/hrithik-roshan.jpg"
+                alt="profile-pic"
+              />
+            </div>
+            <div className="profile-name-bio">
+              <div>
+                <h2>Aditya Kumar</h2>
+              </div>
+              <div>
+                <p>
+                  <i>"In colorful delusions, I keep flying"</i>
+                </p>
+              </div>
+            </div>
           </div>
           <div className="profile-lower">
-            <div className="profile-no-of-tweets"></div>
-            <div className="profile-no-of-posts"></div>
-            <div className="profile-no-of-followers"></div>
-            <div className="profile-no-of-following"></div>
+            <div className="profile-no-of-tweets">
+              <h3>124</h3>
+              <p>tweets</p>
+            </div>
+            <div className="profile-no-of-posts">
+              <h3>156</h3>
+              <p>posts</p>
+            </div>
+            <div className="profile-no-of-followers">
+              <h3>484</h3>
+              <p>followers</p>
+            </div>
+            <div className="profile-no-of-following">
+              <h3>29</h3>
+              <p>following</p>
+            </div>
           </div>
         </div>
       </div>
